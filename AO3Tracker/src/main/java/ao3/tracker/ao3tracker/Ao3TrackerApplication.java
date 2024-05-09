f1488f523e96c01f4e7428dfbc645f35f567a345
tegu1365 <tedi.d.petkova@gmail.com>
2024-05-10 00:15:32 +0300

    Init Project

---
+++ AO3Tracker/src/main/java/ao3/tracker/ao3tracker/Ao3TrackerApplication.java
@@ -0,0 +1,13 @@
+package ao3.tracker.ao3tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ao3TrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ao3TrackerApplication.class, args);
+    }
+
+}
---
+++ AO3Tracker/src/test/java/ao3/tracker/ao3tracker/Ao3TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ao3.tracker.ao3tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ao3TrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
