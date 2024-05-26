907d84fba32c9a5641794b179837bf80b6e06491
tegu1365 <tedi.d.petkova@gmail.com>
2024-05-26 18:19:41 +0300

    Done with User service

--- AO3Tracker/src/main/java/ao3/tracker/ao3tracker/service/UserService.java
+++ AO3Tracker/src/main/java/ao3/tracker/ao3tracker/service/UserService.java
@@ -1,8 +1,31 @@
 package ao3.tracker.ao3tracker.service;
 
+import ao3.tracker.ao3tracker.model.Users;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import ao3.tracker.ao3tracker.repository.UsersRepository;
 @Service
 public class UserService {
-    //private UserRepository userRepository=new UserRepository();
+    private final UsersRepository userRepository;
+
+    @Autowired
+    public UserService(UsersRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
+    public Users createUser(Users user) {
+        return userRepository.save(user);
+    }
+
+    public Users findById(Integer id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public Users updateUser(Users user) {
+        return  userRepository.save(user);
+    }
+
+    public void removeUser(Integer id) {
+        userRepository.deleteById(id);
+    }
 }
