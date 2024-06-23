4c7d431d757f9420b8473e9e7bb49bdfec52ebfb
tegu1365 <tedi.d.petkova@gmail.com>
2024-06-23 16:57:39 +0300

    Collection View component done

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FanficComponent} from "./fanfic/fanfic.component";
+import {CollectionInfoComponent} from "./collection-info/collection-info.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FanficComponent],
+  imports: [RouterOutlet, FanficComponent, CollectionInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
